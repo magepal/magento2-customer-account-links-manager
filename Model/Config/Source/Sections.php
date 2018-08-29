bc28df7caae90d3dc0ce59c3001cdbad898aaa7f
Renon Stewart <s.renon@gmail.com>
2018-08-29 16:02:41 -0400

    Issues #4 - Code refactor

--- Model/Config/Source/Sections.php
+++ Model/Config/Source/Sections.php
@@ -3,7 +3,7 @@
  * Copyright © MagePal LLC. All rights reserved.
  * See COPYING.txt for license details.
  * http://www.magepal.com | support@magepal.com
-*/
+ */
 
 namespace MagePal\CustomerAccountLinksManager\Model\Config\Source;
 
@@ -13,6 +13,8 @@ class Sections implements \Magento\Framework\Option\ArrayInterface
     protected $utilityFiles;
 
     protected $links;
+
+    protected $list = [];
 
     public function __construct(
         \Magento\Framework\App\Utility\Files $utilityFiles,
@@ -36,47 +38,72 @@ class Sections implements \Magento\Framework\Option\ArrayInterface
     {
         $fileList = $this->utilityFiles->getLayoutFiles(['area_name' => 'frontend'], false);
 
-        $list = [];
-
         foreach ($fileList as $configFile) {
             if (strpos($configFile, 'customer_account.xml') !== false) {
                 $configXml = simplexml_load_file($configFile);
+                $this->processXmlElement($configXml);
+            }
+        }
 
-                if ($referenceBlocks = $configXml->body->referenceBlock) {
-                    foreach ($referenceBlocks as $referenceBlock) {
-                        if (!empty($referenceBlock->xpath('block/arguments/argument[@name="label"]'))) {
-                            $list[(string) $referenceBlock->block['name']] = [
-                                'value' => (string) $referenceBlock->block['name'],
-                                'label' => (string) $referenceBlock->xpath('block/arguments/argument[@name="label"]')[0]
-                            ];
-                        }
-                    }
-                }
+        return $this->list;
+    }
 
-                if (isset($configXml->body->referenceContainer)) {
-                    if ($referenceContainerBlocks = $configXml->body->referenceContainer->block->block->block) {
-                        for ($count = 0; $count < count($referenceContainerBlocks); $count++) {
-                            if (!empty($referenceContainerBlocks[$count]->xpath('arguments/argument[@name="label"]'))) {
-                                $list[(string) $referenceContainerBlocks[$count]['name']] = [
-                                    'value' => (string) $referenceContainerBlocks[$count]['name'],
-                                    'label' => (string) $referenceContainerBlocks[$count]->xpath('arguments/argument[@name="label"]')[0]
-                                ];
-                            }
-                        }
-                    } elseif ($referenceContainerBlocks = $configXml->body->referenceContainer->block->block) {
-                        for ($count = 0; $count < count($referenceContainerBlocks); $count++) {
-                            if (!empty($referenceContainerBlocks[$count]->xpath('arguments/argument[@name="label"]'))) {
-                                $list[(string) $referenceContainerBlocks[$count]['name']] = [
-                                    'value' => (string) $referenceContainerBlocks[$count]['name'],
-                                    'label' => (string) $referenceContainerBlocks[$count]->xpath('arguments/argument[@name="label"]')[0]
-                                ];
-                            }
-                        }
-                    }
+    /**
+     * @param $configXml
+     */
+    protected function processXmlElement($configXml)
+    {
+        if ($referenceBlocks = $configXml->body->referenceBlock) {
+            foreach ($referenceBlocks as $referenceBlock) {
+                if (!empty($referenceBlock->xpath('block/arguments/argument[@name="label"]'))) {
+                    $this->updateReferenceBlockList($referenceBlock);
                 }
             }
         }
 
-        return $list;
+        if (isset($configXml->body->referenceContainer) && isset($configXml->body->referenceContainer->block)) {
+            if (isset($configXml->body->referenceContainer->block->block)
+                && isset($configXml->body->referenceContainer->block->block->block)
+            ) {
+                $referenceContainerBlocks = $configXml->body->referenceContainer->block->block->block;
+
+                for ($count = 0; $count < count($referenceContainerBlocks); $count++) {
+                    if (!empty($referenceContainerBlocks[$count]->xpath('arguments/argument[@name="label"]'))) {
+                        $this->updateReferenceContainerList($referenceContainerBlocks, $count);
+                    }
+                }
+            } elseif (isset($configXml->body->referenceContainer->block->block)) {
+                $referenceContainerBlocks = $configXml->body->referenceContainer->block->block;
+
+                for ($count = 0; $count < count($referenceContainerBlocks); $count++) {
+                    if (!empty($referenceContainerBlocks[$count]->xpath('arguments/argument[@name="label"]'))) {
+                        $this->updateReferenceContainerList($referenceContainerBlocks, $count);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * @param $rcb
+     * @param $count
+     */
+    protected function updateReferenceContainerList($rcb, $count)
+    {
+        $this->list[(string) $rcb[$count]['name']] = [
+            'value' => (string) $rcb[$count]['name'],
+            'label' => (string) $rcb[$count]->xpath('arguments/argument[@name="label"]')[0]
+        ];
+    }
+
+    /**
+     * @param $rb
+     */
+    protected function updateReferenceBlockList($rb)
+    {
+        $this->list[(string) $rb->block['name']] = [
+            'value' => (string) $rb->block['name'],
+            'label' => (string) $rb->xpath('block/arguments/argument[@name="label"]')[0]
+        ];
     }
 }
